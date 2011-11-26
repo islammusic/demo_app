fadae05d41866e78354c6c8e7237afee3e1ff97c
Islam Mušić <islam.music@gmail.com>
2011-11-26 13:10:25 +0100

    Zacetni vnos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '001fbc61055f0326f6efacf8503d6cf3a60fd031fb7aa82ea5baf8b6d333f9ae42cafeb51c96f7aae36a453f278c2570e68235de0271cc09b931fad2897822aa'
